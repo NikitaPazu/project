26066907e2e96bd00e16101f6fa5382c0d62342d
NikitaPazu <npazuskin@gmail.com>
2023-04-01 16:29:27 +0400

    Создал начало сайта

---
+++ python_articles/python_articles/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def homePage(request):
+    return HttpResponse('<h1>HOME</h1>')
+def index(request):
+    return render(index, 'index.html')
