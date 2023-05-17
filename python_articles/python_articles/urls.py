bfe6c01c0d8790e40e06f1e731e2443fa3f48a4e
NikitaPazu <npazuskin@gmail.com>
2023-05-17 19:46:12 +0400

    Поэксперементировал с оформлением сайта (5)

--- python_articles/python_articles/urls.py
+++ python_articles/python_articles/urls.py
@@ -5,9 +5,10 @@ from django.urls import path
 urlpatterns = [
     path('', views.homePage),
     path('about/', views.index),
-    path('home>', views.index),
-    path('users_articles>', views.index),
-    path('add_post>', views.index)
+    path('home', views.index),
+    path('users_articles', views.index),
+    path('add_post>', views.index),
+    # path('post/likes/<id>', )
 ]
 
 
--- python_articles/python_articles/views.py
+++ python_articles/python_articles/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 
 def homePage(request):
@@ -8,3 +9,11 @@ def homePage(request):
 
 def index(request):
     return render(index, 'index.html')
+
+
+def delete_product(request, id):
+    post = Post.objects.get(id=id)
+    post.likes += 1
+    post.save()
+    return redirect("/")
+
